5158493d1b020f2885d06bc97f79d7511c2cc710
Sukomal07 <hypersukomal@gmail.com>
2023-07-18 22:36:25 +0530

    created database

---
+++ server/database/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+mongoose.set('strictQuery', false);
+export const connectDb = () => {
+    mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Database connection successfully");
+    })
+    .catch((err) => {
+        console.log(err);
+        process.exit(1)
+    })
+}
+
