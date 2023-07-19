9b2c81c533f7c403cd966a20cb563dc6efcfb71f
Sukomal07 <hypersukomal@gmail.com>
2023-07-19 22:30:35 +0530

    add auth middleware

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { getProfile, login, logout, signup } from "../controller/userController.js";
+import { isLoggedIn } from "../middleware/authMiddleware.js";
 
 const router = express.Router()
 
 router.post("/signup",signup)
 router.post("login", login)
 router.get("/logout", logout)
-router.get("/myprofile", getProfile)
+router.get("/myprofile", isLoggedIn, getProfile)
 
 export default router
