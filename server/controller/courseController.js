32c3717689cd468d7128df7d673d9448bff24c5d
Sukomal07 <hypersukomal@gmail.com>
2023-08-01 21:49:30 +0530

    add course controller

---
+++ server/controller/courseController.js
@@ -0,0 +1,34 @@
+import Course from '../models/courseModel.js';
+import createError from '../utils/error.js'
+export const getAllCourses = async (req, res, next) => {
+    try {
+        const courses = await Course.find({}).select('-lectures');
+        if (!courses) {
+            return next(createError(404, "No courses found"))
+        }
+        res.status(200).json({
+            success: true,
+            message: "All courses",
+            courses
+        })
+    } catch (error) {
+        return next(createError(500, error.message))
+    }
+}
+
+export const getLectures = async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const course = await Course.findById(id)
+        if (!courses) {
+            return next(createError(404, "No courses found"))
+        }
+        res.status(200).json({
+            success: true,
+            message: "Lectures fetched successfully",
+            lectures: course.lectures
+        })
+    } catch (error) {
+        return next(createError(500, error.message))
+    }
+}
