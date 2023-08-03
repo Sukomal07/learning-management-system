2a87f36ed640c3afd9a2dabacf6748d1a22b1b03
Sukomal07 <hypersukomal@gmail.com>
2023-08-03 23:30:21 +0530

    add all routes

--- server/routes/courseRoutes.js
+++ server/routes/courseRoutes.js
@@ -1,13 +1,16 @@
 import { Router } from 'express'
-import { createCourse, deleteCourse, getAllCourses, getLectures, updateCourse } from '../controller/courseController.js'
-import { isLoggedIn } from "../middleware/authMiddleware.js";
+import { addLecturesToCourse, createCourse, deleteCourse, deleteLectures, getAllCourses, getLectures, updateCourse, updateLectures } from '../controller/courseController.js'
+import { authorizedRole, isLoggedIn } from "../middleware/authMiddleware.js";
+import upload from '../middleware/multer.js'
 const router = Router()
 
 router.get('/', getAllCourses)
-router.post('/', isLoggedIn, createCourse)
-router.put('/:id', isLoggedIn, updateCourse)
-router.put('/:id', isLoggedIn, deleteCourse)
+router.post('/newcourse', isLoggedIn, authorizedRole('ADMIN'), upload.single("thumbnail"), createCourse)
+router.put('/:id', isLoggedIn, authorizedRole('ADMIN'), upload.single("thumbnail"), updateCourse)
+router.delete('/:id', isLoggedIn, authorizedRole('ADMIN'), deleteCourse)
 router.get("/:id", isLoggedIn, getLectures)
-
+router.post('/:id', isLoggedIn, authorizedRole('ADMIN'), upload.single("lecture"), addLecturesToCourse)
+router.put('/lectures/:id/:lectureId', isLoggedIn, authorizedRole('ADMIN'), upload.single("lecture"), updateLectures)
+router.delete('/lectures/:id/:lectureId', isLoggedIn, authorizedRole('ADMIN'), deleteLectures)
 
 export default router
