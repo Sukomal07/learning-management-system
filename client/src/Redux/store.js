257bdbb78d98543b872fd97b45fada56dd911ce5
Sukomal07 <hypersukomal@gmail.com>
2023-08-31 11:14:38 +0530

    create strore

---
+++ client/src/Redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+const store = configureStore({
+    reducer: {},
+    devTools: true
+})
+
+
+export default store
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,19 @@
 import './index.css'
 
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App.jsx'
-
+import store from './Redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
       <Toaster />
     </BrowserRouter>
-  </React.StrictMode>,
+  </Provider>
+
 )
