7d85a543c1224e122fab2b5213fbd367912e71c9
Sukomal07 <hypersukomal@gmail.com>
2023-09-09 12:15:22 +0530

    importe carousel in about

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -1,14 +1,13 @@
-import aboutMainImage from '../assets/aboutMainImage.png'
-import apj from '../assets/apj.png'
-import billGates from '../assets/billGates.png'
-import einstein from '../assets/einstein.png'
-import nelsonMandela from '../assets/nelsonMandela.png'
-import steveJobs from '../assets/steveJobs.png'
+import aboutMainImage from '../assets/Images/aboutMainImage.png'
+import option2 from '../assets/json/option2.json'
+import Carousel from '../components/Carousel'
+import Particle from '../components/Particle'
+import celeb from '../constants/celebData.js'
 import HomeLayout from '../Layouts/HomeLayout'
-
 function About() {
     return (
         <HomeLayout>
+            <Particle option={option2} />
             <div className=' flex flex-col  lg:p-16 p-8'>
                 <section className='flex lg:flex-row flex-col items-center justify-between w-full'>
                     <div className=' flex flex-col gap-16 lg:w-[70%] w-full'>
@@ -18,67 +17,13 @@ function About() {
                             their skills, creativity and knowledge to each other to empower and contribute
                             in the growth and wellness of mankind.  </p>
                     </div>
-                    <div className='lg:w-[30%] drop-shadow-2xl brightness-50 '>
+                    <div className='lg:w-[30%] drop-shadow-2xl '>
                         <img src={aboutMainImage} alt="image" className='bg-transparent' />
                     </div>
                 </section>
                 <section className='pt-4 w-full lg:w-[80%] lg:mx-auto'>
                     <div className="carousel w-full">
-                        <div id="slide1" className="carousel-item relative w-full justify-center">
-                            <div className='flex flex-col gap-5 justify-center items-center'>
-                                <img src={apj} className="w-40 h-40 lg:w-[300px] lg:h-[300px] rounded-3xl" />
-                                <p className='lg:text-xl text-[1rem] text-slate-500 text-center'>" If you want to shine like a sun , first burn like a sun. "</p>
-                                <h3 className='font-bold text-center text-white text-xl'>APJ Abdul Kalam</h3>
-                            </div>
-                            <div className="absolute flex justify-between transform -translate-y-1/2 lg:left-5 lg:right-5 left-1 right-1 top-1/2">
-                                <a href="#slide5" className="btn btn-circle">❮</a>
-                                <a href="#slide2" className="btn btn-circle">❯</a>
-                            </div>
-                        </div>
-                        <div id="slide2" className="carousel-item relative w-full justify-center">
-                            <div className='flex flex-col gap-5 justify-center items-center'>
-                                <img src={billGates} className="w-40 h-40 lg:w-[300px] lg:h-[300px] rounded-3xl" />
-                                <p className='lg:text-xl text-[1rem] text-slate-500 text-center'>" Patience is a key element of success. "</p>
-                                <h3 className='font-bold text-center text-white text-xl'>Bill Gates</h3>
-                            </div>
-                            <div className="absolute flex justify-between transform -translate-y-1/2 lg:left-5 lg:right-5 left-1 right-1 top-1/2">
-                                <a href="#slide1" className="btn btn-circle">❮</a>
-                                <a href="#slide3" className="btn btn-circle">❯</a>
-                            </div>
-                        </div>
-                        <div id="slide3" className="carousel-item relative w-full justify-center">
-                            <div className='flex flex-col gap-5 justify-center items-center'>
-                                <img src={einstein} className="w-40 h-40 lg:w-[300px] lg:h-[300px] rounded-3xl" />
-                                <p className='lg:text-xl text-[1rem] text-slate-500 text-center'>" Try no to become a man of success, but rather try to become a man of value. "</p>
-                                <h3 className='font-bold text-center text-white text-xl'>Albert Einstein</h3>
-                            </div>
-                            <div className="absolute flex justify-between transform -translate-y-1/2 lg:left-5 lg:right-5 left-1 right-1 top-1/2">
-                                <a href="#slide2" className="btn btn-circle">❮</a>
-                                <a href="#slide4" className="btn btn-circle">❯</a>
-                            </div>
-                        </div>
-                        <div id="slide4" className="carousel-item relative w-full justify-center">
-                            <div className='flex flex-col gap-5 justify-center items-center'>
-                                <img src={nelsonMandela} className="w-40 h-40 lg:w-[300px] lg:h-[300px] rounded-3xl" />
-                                <p className='lg:text-xl text-[1rem] text-slate-500 text-center'>" Education is the most powerful tool you can use to change the world. "</p>
-                                <h3 className='font-bold text-center text-white text-xl'>Nelson Mendela</h3>
-                            </div>
-                            <div className="absolute flex justify-between transform -translate-y-1/2 lg:left-5 lg:right-5 left-1 right-1 top-1/2">
-                                <a href="#slide3" className="btn btn-circle">❮</a>
-                                <a href="#slide5" className="btn btn-circle">❯</a>
-                            </div>
-                        </div>
-                        <div id="slide5" className="carousel-item relative w-full justify-center">
-                            <div className='flex flex-col gap-5 justify-center items-center'>
-                                <img src={steveJobs} className="w-40 h-40 lg:w-[300px] lg:h-[300px] rounded-3xl" />
-                                <p className='lg:text-xl text-[1rem] text-slate-500 text-center'>" Quality is more important than quantity. "</p>
-                                <h3 className='font-bold text-center text-white text-xl'>Steve Jobs</h3>
-                            </div>
-                            <div className="absolute flex justify-between transform -translate-y-1/2 lg:left-5 lg:right-5 left-1 right-1 top-1/2">
-                                <a href="#slide4" className="btn btn-circle">❮</a>
-                                <a href="#slide1" className="btn btn-circle">❯</a>
-                            </div>
-                        </div>
+                        {celeb && celeb.map(person => <Carousel key={person.slidenumber} {...person} totalslide={celeb.length} />)}
                     </div>
                 </section>
             </div>
