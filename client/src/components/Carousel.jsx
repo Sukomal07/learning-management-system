9254362200373f71259d5849d4f0e7a656d5e3cc
Sukomal07 <hypersukomal@gmail.com>
2023-09-09 12:14:50 +0530

    create carousel

---
+++ client/src/components/Carousel.jsx
@@ -0,0 +1,18 @@
+
+function Carousel({ image, title, desc, slidenumber, totalslide }) {
+    return (
+        <div id={`slide${slidenumber}`} className="carousel-item relative w-full justify-center">
+            <div className='flex flex-col gap-5 justify-center items-center'>
+                <img src={image} className="w-40 h-40 lg:w-[300px] lg:h-[300px] rounded-3xl" />
+                <p className='lg:text-xl text-[1rem] text-slate-500 text-center'>{desc}</p>
+                <h3 className='font-bold text-center text-white text-xl'>{title}</h3>
+            </div>
+            <div className="absolute flex justify-between transform -translate-y-1/2 lg:left-5 lg:right-5 left-1 right-1 top-1/2">
+                <a href={`#slide${slidenumber === 1 ? totalslide : slidenumber - 1}`} className="btn btn-circle">❮</a>
+                <a href={`#slide${slidenumber === totalslide ? 1 : slidenumber + 1}`} className="btn btn-circle">❯</a>
+            </div>
+        </div>
+    )
+}
+
+export default Carousel
---
+++ client/src/constants/celebData.js
@@ -0,0 +1,38 @@
+import apj from '../assets/Images/apj.png'
+import billGates from '../assets/Images/billGates.png'
+import einstein from '../assets/Images/einstein.png'
+import nelsonMandela from '../assets/Images/nelsonMandela.png'
+import steveJobs from '../assets/Images/steveJobs.png'
+
+export const celeb = [
+    {
+        slidenumber: 1,
+        image: apj,
+        title: "APJ Abdul Kalam",
+        desc: " If you want to shine like a sun , first burn like a sun. "
+    },
+    {
+        slidenumber: 2,
+        image: billGates,
+        title: "Bill Gates",
+        desc: " Patience is a key element of success. "
+    },
+    {
+        slidenumber: 3,
+        image: einstein,
+        title: "Albert Einstein",
+        desc: " Try no to become a man of success, but rather try to become a man of value. "
+    },
+    {
+        slidenumber: 4,
+        image: nelsonMandela,
+        title: "Nelson Mendela",
+        desc: " Education is the most powerful tool you can use to change the world. "
+    },
+    {
+        slidenumber: 5,
+        image: steveJobs,
+        title: "Steve Jobs",
+        desc: " Quality is more important than quantity. "
+    }
+]
