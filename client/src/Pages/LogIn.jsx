fd1f1a42c59711be41336e04970112a0e3df6dcf
Sukomal07 <hypersukomal@gmail.com>
2023-09-11 20:49:34 +0530

    login

--- client/src/Pages/LogIn.jsx
+++ client/src/Pages/LogIn.jsx
@@ -1,15 +1,42 @@
+import { useState } from "react"
 import { BsEnvelope, BsLock } from "react-icons/bs"
-import { Link } from "react-router-dom"
+import { useDispatch } from "react-redux"
+import { Link, useNavigate } from "react-router-dom"
 
 import option2 from '../assets/Json/option2.json'
 import Particle from "../components/Particle"
 import HomeLayout from "../layouts/HomeLayout"
+import { login } from "../redux/slices/AuthSlice"
 function LogIn() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [logInData, setLogInData] = useState({
+        email: "",
+        password: ""
+    })
+    function handleUserInput(e) {
+        const { name, value } = e.target;
+        setLogInData({ ...logInData, [name]: value })
+    }
+
+    async function onLogin(event) {
+        event.preventDefault();
+        const response = await dispatch(login(logInData))
+        if (response.payload?.success) {
+            navigate('/');
+            setLogInData({
+                email: "",
+                password: ""
+            })
+        }
+    }
+
     return (
         <HomeLayout>
             <Particle option={option2} />
             <div className='flex flex-col gap-3 justify-center items-center h-screen'>
-                <form className='lg:w-[450px] w-[90%] md:w-1/2 h-[60%] p-7 flex flex-col justify-between rounded-md bg-white text-black shadow-md'>
+                <form onSubmit={onLogin} className='lg:w-[450px] w-[90%] md:w-1/2 h-[60%] p-7 flex flex-col justify-between rounded-md bg-white text-black shadow-md'>
                     <div>
                         <h1 className='text-3xl font-semibold mb-3'>Log In</h1>
                         <p className='text-slate-400'>Please fill this form to login your account</p>
@@ -17,11 +44,11 @@ function LogIn() {
                     <hr className='border-t-2 border-slate-500' />
                     <div className='flex items-center w-full gap-4 border-2 border-yellow-500 px-4 rounded-lg h-14 bg-slate-900'>
                         <label htmlFor="email" className='text-xl hidden lg:block md:block text-yellow-500'><BsEnvelope /></label>
-                        <input type="email" name="email" id="email" placeholder='Enter Email' className="py-2 border-0 outline-0 text-xl text-white bg-transparent w-full " />
+                        <input type="email" name="email" id="email" placeholder='Enter Email' className="py-2 border-0 outline-0 text-xl text-white bg-transparent w-full " value={logInData.email} onChange={handleUserInput} />
                     </div>
                     <div className='flex items-center w-full gap-4 border-2 border-yellow-500 px-4 rounded-lg h-14 bg-slate-900'>
                         <label htmlFor="password" className='text-sm hidden lg:block md:block text-yellow-500'><BsLock /></label>
-                        <input type="password" name="password" id="password" placeholder='Enter Password' className="py-2 border-0 outline-0 text-xl text-white bg-transparent w-full " />
+                        <input type="password" name="password" id="password" placeholder='Enter Password' className="py-2 border-0 outline-0 text-xl text-white bg-transparent w-full " value={logInData.password} onChange={handleUserInput} />
                     </div>
                     <button type='submit' className='btn btn-primary w-full  mx-auto'>LogIn</button>
                 </form>
--- client/src/Redux/slices/AuthSlice.js
+++ client/src/Redux/slices/AuthSlice.js
@@ -23,13 +23,45 @@ export const signup = createAsyncThunk("/auth/signup", async (data) => {
         toast.error(error?.response?.data?.message);
     }
 });
+export const login = createAsyncThunk("/auth/login", async (data) => {
+    try {
+        toast.loading("Wait! login in your account", {
+            position: 'top-center'
+        });
+        const response = await axiosInstance.post('/user/login', data);
+        toast.dismiss();
+        toast.success(response.data.message);
+        return response.data;
+    } catch (error) {
+        toast.dismiss();
+        toast.error(error?.response?.data?.message);
+    }
+});
 
 
 
 const authSlice = createSlice({
     name: 'auth',
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(login.fulfilled, (state, action) => {
+            localStorage.setItem("isLoggedIn", true);
+            localStorage.setItem("data", JSON.stringify(action?.payload?.userData));
+            localStorage.setItem("role", action?.payload?.userData?.role);
+            state.isLoggedIn = true
+            state.data = JSON.stringify(action?.payload?.userData)
+            state.role = action?.payload?.userData?.role
+        })
+        builder.addCase(signup.fulfilled, (state, action) => {
+            localStorage.setItem("isLoggedIn", true);
+            localStorage.setItem("data", JSON.stringify(action?.payload?.user));
+            localStorage.setItem("role", action?.payload?.user?.role);
+            state.isLoggedIn = true
+            state.data = JSON.stringify(action?.payload?.user)
+            state.role = action?.payload?.user?.role
+        })
+    }
 })
 
 
