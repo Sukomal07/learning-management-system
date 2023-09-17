b897da4844c863cb3dac7391b8df9f19baab3034
Sukomal07 <hypersukomal@gmail.com>
2023-09-17 22:41:28 +0530

    create course

--- client/src/Pages/course/CreateCourse.jsx
+++ client/src/Pages/course/CreateCourse.jsx
@@ -1,9 +1,69 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify';
 
+import HomeLayout from '../../layouts/HomeLayout'
+import { createCourse } from '../../redux/slices/CourseSlice';
 function CreateCourse() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [userInput, setUserInput] = useState({
+        title: "",
+        description: "",
+        category: "",
+        createdBy: "",
+        thumbnail: null,
+        previewImage: ""
+    })
+
+    function handleImage(e) {
+        e.preventDefault();
+        const uploadImage = e.target.files[0];
+        if (uploadImage) {
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(uploadImage)
+            fileReader.addEventListener('load', function () {
+                setUserInput({
+                    ...userInput,
+                    previewImage: this.result,
+                    thumbnail: uploadImage
+                })
+            })
+        }
+    }
+
+    async function onSubmit(e) {
+        e.preventDefault();
+        if (!userInput.thumbnail) {
+            toast.error("Please enter course thumbnail")
+            return;
+        }
+        const formData = new FormData();
+        formData.append("title", userInput.title);
+        formData.append("description", userInput.description);
+        formData.append("category", userInput.category);
+        formData.append("createdBy", userInput.createdBy);
+        formData.append("thumbnail", userInput.thumbnail);
+
+        const response = await dispatch(createCourse(formData));
+        if (response.payload?.success) {
+            navigate('/courses');
+            setUserInput({
+                title: "",
+                description: "",
+                category: "",
+                createdBy: "",
+                thumbnail: null,
+                previewImage: ""
+            })
+        }
+    }
+
     return (
-        <div>
-            hello
-        </div>
+        <HomeLayout>
+
+        </HomeLayout>
     )
 }
 
--- client/src/Redux/slices/CourseSlice.js
+++ client/src/Redux/slices/CourseSlice.js
@@ -19,6 +19,29 @@ export const getAllCourse = createAsyncThunk('/course/get', async () => {
     } catch (error) {
         toast.dismiss();
         toast.error(error?.response?.statusText);
+        throw error;
+    }
+})
+
+export const createCourse = createAsyncThunk('/course/create', async (data) => {
+    try {
+        toast.loading("wait! creating course...", {
+            position: 'top-center'
+        })
+        const response = await axiosInstance.post('/course/create', data);
+        if (response.status === 201) {
+            toast.dismiss();
+            toast.success(response.data.message);
+            return response.data;
+        } else {
+            toast.dismiss();
+            toast.error(response.data.message);
+            throw new Error(response.data.message);
+        }
+    } catch (error) {
+        toast.dismiss();
+        toast.error(error?.response?.data?.message);
+        throw error;
     }
 })
 
