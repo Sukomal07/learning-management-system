8d9fdb1d6f864edebb2c19e97735d2b461a1cd8a
Sukomal07 <hypersukomal@gmail.com>
2023-09-30 13:15:14 +0530

    update package

--- client/src/Pages/payments/CheckoutFail.jsx
+++ client/src/Pages/payments/CheckoutFail.jsx
@@ -1,5 +1,5 @@
+import Lottie from 'lottie-react'
 import { useEffect } from 'react';
-import Lottie from 'react-lottie'
 import { useDispatch } from 'react-redux';
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 
@@ -21,18 +21,10 @@ function CheckoutFail() {
             onLoad()
         }
     }, [])
-    const defaultOptions = {
-        loop: true,
-        autoplay: true,
-        animationData: animationData,
-        rendererSettings: {
-            preserveAspectRatio: "xMidYMid slice"
-        }
-    };
     return (
         <div className='h-screen flex justify-center items-center'>
             <div className='lg:w-1/3 w-11/12 m-auto bg-white rounded-lg shadow-lg flex flex-col gap-4 justify-center items-center pb-4'>
-                <Lottie options={defaultOptions} height={300} width={300} />
+                <Lottie animationData={animationData} loop={true} height={300} width={300} />
                 <p className='px-4 text-xl tracking-wider text-slate-500 text-center'>Oops ! Payment is not Successful</p>
                 <Link className='btn btn-primary w-[90%]' to={'/courses'}>Try again</Link>
             </div>
